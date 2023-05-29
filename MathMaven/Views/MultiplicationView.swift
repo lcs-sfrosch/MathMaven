3248f5cd6364b8a732f330713e444d78096af953
Sebastian Frosch <sfrosch@lcs.on.ca>
2023-05-28 20:26:12 -0400

    looked some of the giving code and changed subtraction multiplication

--- MathMaven/Views/MultiplicationView.swift
+++ MathMaven/Views/MultiplicationView.swift
@@ -12,8 +12,123 @@ struct MultiplicationView: View {
         VStack {
             Text("Multiplication")
             Text(Operation.multiplication.rawValue)
+        
+        .font(Font.custom("SF Pro", size: 64))
+
+    
+    @State var firstValue = Int.random(in: 1...144)
+    @State var secondValue = 0  // Will be updated once view loads
+    
+    // Holds whatever input was provided by the user
+    @State var input = ""
+    
+    // Has the user's answer been checked?
+    @State var answerChecked = false
+    
+    // Was the user's given answer correct?
+    @State var answerCorrect = false
+    
+    // MARK: Computed properties
+
+    // The correct response
+    var correctResponse: Int {
+        return firstValue / secondValue
+    }
+    
+    // The user interface
+    var body: some View {
+        VStack(spacing: 0) {
+            
+            
+            Group {
+                HStack {
+                    Text(Operation.multiplication.rawValue)
+                    
+                    Spacer()
+                    
+                    VStack(alignment: .trailing) {
+                        Text("\(firstValue)")
+                        Text("\(secondValue)")
+                    }
+                }
+                
+                Divider()
+            }
+            .padding(.horizontal)
+
+           
+            HStack {
+
+                ZStack {
+                    
+                   
+                    if answerCorrect == true {
+                        Image(systemName: "checkmark.circle")
+                            .foregroundColor(.green)
+                    }
+                    
+                    
+                    if answerChecked == true && answerCorrect == false {
+                        Image(systemName: "x.square")
+                            .foregroundColor(.red)
+                    }
+                }
+                
+                Spacer()
+                
+                TextField("",
+                          text: $input)
+                    .multilineTextAlignment(.trailing)
+            }
+            .padding(.horizontal)
+
+            
+            if answerChecked == false {
+                
+                CheckAnswerButtonView(input: input,
+                                      correctResponse: correctResponse,
+                                      answerChecked: $answerChecked,
+                                      answerCorrect: $answerCorrect)
+                
+            } else {
+                
+              
+                Button(action: {
+                    generateNewQuestion()
+                }, label: {
+                    Text("New question")
+                        .font(.largeTitle)
+                })
+                .padding()
+                .buttonStyle(.bordered)
+                
+            }
+            
+            Spacer()
+
         }
         .font(Font.custom("SF Pro", size: 64))
+    
+        .task {
+            
+            secondValue = Int.random(in: 1...firstValue)
+        }
+    }
+    
+    
+    func generateNewQuestion() {
+        
+      
+        firstValue = Int.random(in: 1...144)
+        secondValue = Int.random(in: 1...firstValue)
+
+        
+        answerChecked = false
+        answerCorrect = false
+        
+       
+        input = ""
+
     }
 }
 
